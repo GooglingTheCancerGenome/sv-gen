69e7fb8db52b8cd18f924877753c6909794b2ff2
Arnold Kuzniar <a.kuzniar@esciencecenter.nl>
2020-03-11 15:50:03 +0100

    Update validator: add output #25.

--- snakemake/validate_conf.py
+++ snakemake/validate_conf.py
@@ -1,9 +1,10 @@
 from ruamel import yaml
 from typing import Dict, List, Union
 import yatiml
+import enum
 
 
-# Create document class
+# Create document classes
 class Input:
     def __init__(
             self,
@@ -12,15 +13,32 @@ class Input:
         self.fasta = fasta
         self.seqids = seqids
 
+
+class Genotype(enum.Enum):
+    HOMO_NOSV = 'hmz'
+    HOMO_SV = 'hmz-sv'
+    HETERO_SV = 'htz-sv'
+
+
+class Output:
+    def __init__(
+            self,
+            basedir: str,
+            genotype: List[str]) -> None:
+        self.basedir = basedir
+        self.genotype = genotype
+
+
 class Analysis:
-    def __init__(self, input: Input) -> None:
+    def __init__(self, input: Input, output: Output) -> None:
         self.input = input
+        self.output = output
 
 # Create loader
 class MyLoader(yatiml.Loader):
     pass
 
-yatiml.add_to_loader(MyLoader, [Input, Analysis])
+yatiml.add_to_loader(MyLoader, [Input, Genotype, Output, Analysis])
 yatiml.set_document_type(MyLoader, Analysis)
 
 # Load YAML
@@ -28,9 +46,17 @@ yaml_text = """
 input:
     fasta: data/chr22_44-45Mb.GRCh37.fasta
     seqids: [22]
+output:
+  basedir: data/out
+  genotype:
+    - hmz
+    - hmz-sv
+    - htz-sv
 """
 doc = yaml.load(yaml_text, Loader=MyLoader)
 print(type(doc))
 print(doc)
 print(doc.input.fasta)
 print(doc.input.seqids)
+print(doc.output.basedir)
+print(doc.output.genotype)
