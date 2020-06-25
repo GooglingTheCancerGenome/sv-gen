fa4108f02741880657ac50fa78ad517feb35bdc1
arnikz <a.kuzniar@esciencecenter.nl>
2020-06-25 19:08:30 +0200

    Update tests: KIS #42.

--- snakemake/tests/test_validator.py
+++ snakemake/tests/test_validator.py
@@ -2,30 +2,29 @@
 import pytest
 import pyfaidx
 
-from validator import load_configfile, SeqIdCountError, SeqIdNotFoundError, \
-                      SimulateSvTypeError, SimulateTransError, FastaFilextError
+from validator import load_configfile
 
 
-def test_input__countseqid_exception(shared_datadir):
-    with pytest.raises(SeqIdCountError):
+def test_input__seqidcount_exception(shared_datadir):
+    with pytest.raises(ValueError):
         load_configfile(shared_datadir / 'analysis_seqidcount_error.yaml')
 
 
 def test_input__seqidnotfound_exception(shared_datadir):
-    with pytest.raises(SeqIdNotFoundError):
+    with pytest.raises(ValueError):
         load_configfile(shared_datadir / 'analysis_seqidnotfound_error.yaml')
 
 
 def test_input__fastafilext_exception(shared_datadir):
-    with pytest.raises(FastaFilextError):
+    with pytest.raises(ValueError):
         load_configfile(shared_datadir / 'analysis_fastafilext_error.yaml')
 
 
 def test_simulation__svtype_exception(shared_datadir):
-    with pytest.raises(SimulateSvTypeError):
+    with pytest.raises(ValueError):
         load_configfile(shared_datadir / 'analysis_svtype_error.yaml')
 
 
 def test_simulation__translocation_exception(shared_datadir):
-    with pytest.raises(SimulateTransError):
+    with pytest.raises(ValueError):
         load_configfile(shared_datadir / 'analysis_trans_error.yaml')
