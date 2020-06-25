02a4738f68d400d8dd34e3c3b248f32fdc9c5f00
arnikz <a.kuzniar@esciencecenter.nl>
2020-06-25 18:16:52 +0200

    Update tests: *.yaml files in shared_datadir #42.

--- snakemake/tests/test_validator.py
+++ snakemake/tests/test_validator.py
@@ -1,4 +1,5 @@
 """Test suite for validator/__init__.py."""
+import os
 import pytest
 import pyfaidx
 
@@ -6,26 +7,26 @@ from validator import load_configfile, SeqIdCountError, SeqIdNotFoundError, \
                       SimulateSvTypeError, SimulateTransError, FastaFilextError
 
 
-def test_input__countseqid_exception():
+def test_input__countseqid_exception(shared_datadir):
     with pytest.raises(SeqIdCountError):
-        load_configfile('tests/analysis_seqidcount_error.yaml')
+        load_configfile(shared_datadir / 'analysis_seqidcount_error.yaml')
 
 
-def test_input__seqidnotfound_exception():
+def test_input__seqidnotfound_exception(shared_datadir):
     with pytest.raises(SeqIdNotFoundError):
-        load_configfile('tests/analysis_seqidnotfound_error.yaml')
+        load_configfile(shared_datadir / 'analysis_seqidnotfound_error.yaml')
 
 
-def test_input__fastafilext_exception():
+def test_input__fastafilext_exception(shared_datadir):
     with pytest.raises(FastaFilextError):
-        load_configfile('tests/analysis_fastafilext_error.yaml')
+        load_configfile(shared_datadir / 'analysis_fastafilext_error.yaml')
 
 
-def test_simulation__svtype_exception():
+def test_simulation__svtype_exception(shared_datadir):
     with pytest.raises(SimulateSvTypeError):
-        load_configfile('tests/analysis_svtype_error.yaml')
+        load_configfile(shared_datadir / 'analysis_svtype_error.yaml')
 
 
-def test_simulation__translocation_exception():
+def test_simulation__translocation_exception(shared_datadir):
     with pytest.raises(SimulateTransError):
-        load_configfile('tests/analysis_trans_error.yaml')
+        load_configfile(shared_datadir / 'analysis_trans_error.yaml')
