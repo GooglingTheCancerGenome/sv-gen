b31c093a3f94c1a53d2b4cac46bcd75af0e58f99
Arnold Kuzniar <arnold.kuzniar@surf.nl>
2022-12-09 20:31:28 +0100

    Fix config path.

--- run.sh
+++ run.sh
@@ -4,12 +4,13 @@ set -e
 
 SCH=$1
 MY_ENV=wf
+CONFIG="../config/analysis.yaml"
 
 eval "$(conda shell.bash hook)"
 conda activate $MY_ENV
 conda list
 cd ./workflow && ls -alh
-snakemake --use-conda --configfile analysis.yaml \
+snakemake --use-conda --configfile $CONFIG \
   --latency-wait 60 --jobs \
   --cluster "xenon -vvv scheduler $SCH --location local:// submit \
     --name smk.{rule} --inherit-env --max-run-time 5 --working-directory . \
